66cb5379f2e1e9f07d15b01a824eaab85a578177
Sergin-cat <111153675+Sergin-cat@users.noreply.github.com>
2026-01-01 16:54:36 +0200

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,50 @@
+export const cardArray = [
+    {
+        id: 0,
+        title: 'Первый товар',
+        price: '170 $',
+        address: 'Коростень, р-н Березовки',
+        date: '10 июля 11:50',
+        img: '/image/pvx-image.jpg'
+    },
+    {
+        id: 1,
+        title: 'Второй товар',
+        price: '171 $',
+        address: 'Коростень, р-н Березовки',
+        date: '10 июля 11:50',
+        img: '/image/pvx-image.jpg'
+    },
+    {
+        id: 2,
+        title: 'Третий товар',
+        price: '172 $',
+        address: 'Коростень, р-н Березовки',
+        date: '10 июля 11:50',
+        img: '/image/pvx-image.jpg'
+    },
+    {
+        id: 3,
+        title: 'Четвертый товар',
+        price: '173 $',
+        address: 'Коростень, р-н Березовки',
+        date: '10 июля 11:50',
+        img: '/image/pvx-image.jpg'
+    },
+    {
+        id: 4,
+        title: 'Пятый товар',
+        price: '174 $',
+        address: 'Коростень, р-н Березовки',
+        date: '10 июля 11:50',
+        img: '/image/pvx-image.jpg'
+    },
+    {
+        id: 5,
+        title: 'Шестой товар',
+        price: '175 $',
+        address: 'Коростень, р-н Березовки',
+        date: '10 июля 11:50',
+        img: '/image/pvx-image.jpg'
+    }
+]
